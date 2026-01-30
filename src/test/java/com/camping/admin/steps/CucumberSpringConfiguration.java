400cd86a876e656586b9828baf141e8c68bdf7f6
yhh1056 <hyeonho9995@gmail.com>
2026-01-30 16:54:27 +0900

    MySQL 설정 추가, 환경 분리

--- src/test/java/com/camping/admin/steps/CucumberSpringConfiguration.java
+++ src/test/java/com/camping/admin/steps/CucumberSpringConfiguration.java
@@ -2,11 +2,13 @@ package com.camping.admin.steps;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = "spring.sql.init.mode=never" // data.sql 실행 방지
 )
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
 }
